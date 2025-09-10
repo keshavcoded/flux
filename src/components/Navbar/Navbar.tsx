21a51872dbfa87fecffcbde3db90e61cef6d84f4
keshavcoded <keshav.shri06@gmail.com>
2025-09-10 12:43:22 +0530

    [feat]: server action sync auth service and db

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
 import Desktop from "./Desktop";
 import Mobile from "./Mobile";
+import { currentUser } from "@clerk/nextjs/server";
+import { syncUser } from "@/actions/user.action";
 
-function Navbar() {
+async function Navbar() {
+  const user = await currentUser();
+  if (user) await syncUser();
+
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backfrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
